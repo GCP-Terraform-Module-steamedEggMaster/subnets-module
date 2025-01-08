d79ec2c5e91903f63cf49da6ae4fc25ee0348a80
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-08 16:42:28 +0900

    fix : modify vpc code

--- test/helpers/vpc.go
+++ test/helpers/vpc.go
@@ -9,19 +9,16 @@ import (
 // VPC를 생성하고 VPC ID와 정리 함수를 반환합니다.
 func CreateVPC(t *testing.T, projectID string) (string, func()) {
 	vpcOptions := &terraform.Options{
-		// GitHub에 있는 VPC 모듈 경로 설정
-		TerraformDir: "./vpc",
-
-		// VPC 모듈 입력 변수 설정
+		// Terraform 모듈 입력 변수 설정
 		Vars: map[string]interface{}{
 			"project_id":       projectID,
 			"vpc_network_name": "test-vpc",
 			"routing_mode":     "REGIONAL",
 		},
 
-		// GitHub에서 VPC 모듈을 가져오기 위한 경로
-		BackendConfig: map[string]interface{}{
-			"source": "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0",
+		// 원격 모듈 소스 설정
+		EnvVars: map[string]string{
+			"TF_VAR_source": "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0",
 		},
 	}
 
