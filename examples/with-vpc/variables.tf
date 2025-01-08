b426cbf2bc2b05f16024b3ab0f8f01524fb32852
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-08 18:25:41 +0900

    fix : remove network_id variables

--- examples/with-vpc/variables.tf
+++ examples/with-vpc/variables.tf
@@ -15,11 +15,6 @@ variable "subnet_region" {
   type        = string
 }
 
-variable "network_id" {
-  description = "The ID of the VPC network where this subnetwork will be created"
-  type        = string
-}
-
 variable "subnet_ip_cidr_range" {
   description = "The CIDR range of the subnet"
   type        = string
