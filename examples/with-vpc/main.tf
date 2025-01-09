ac776fe8699c30fb21f75509dc2ef5988b5fd085
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-10 00:27:19 +0900

    fix : modify vpc var argument

--- examples/with-vpc/main.tf
+++ examples/with-vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0"
 
-  vpc_network_name = var.vpc_network_name
+  name = "test-vpc"
 }
 
 module "subnet" {
